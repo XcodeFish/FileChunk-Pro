c127d4763cb230c465158ed80f532ed31b7ba821
angel <passerby365@163.com>
2025-06-04 22:56:41 +0800

    fix: 修复eslinttrc报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,7 @@ module.exports = {
     project: './tsconfig.json'
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   env: {
     browser: true,
     node: true,
@@ -22,11 +18,20 @@ module.exports = {
     'prettier/prettier': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-console': ['warn', { allow: ['warn', 'error'] }]
   },
   overrides: [
+    {
+      files: ['.eslintrc.js', '*.config.js', 'jest.config.js', 'rollup.config.js'],
+      parser: 'espree',
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: null
+      }
+    },
     {
       files: ['*.test.ts', '*.spec.ts'],
       rules: {
@@ -34,4 +39,4 @@ module.exports = {
       }
     }
   ]
-}; 
+};
